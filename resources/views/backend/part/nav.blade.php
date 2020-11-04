b050b06b0ce6fb96b93301d59ab752d1694c204d
Yui Nakorn <yuinakorn@hotmail.com>
2020-11-05 00:33:51 +0700

    adj some css

--- resources/views/backend/part/nav.blade.php
+++ resources/views/backend/part/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">
-        <i class="fa fa-chart-pie"></i> CMH :
-        <small class="text-success"> Dashboard</small>
+        <i class="fa fa-chart-pie"></i> CM 
+        <span class="text-success"> Dashboard</span>
         <small class="text-secondary">: Admin Mode</small>
     </a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
-@section('title',"CMH :: DASHBOARD")
+@section('title',"CM DASHBOARD")
 @section('content')
 
-<div class="col-lg-12">
-    <h2>ระบบรายงานข้อมูล</h2>
+<div class="col-lg-12 idx-main">
+    <div class="idx-title">
+        <h2>รายงานด้านการแพทย์ฉุกเฉินและสาธารณสุข</h2>
+    </div>
     <div class="col-lg-12" style="text-align: center;margin: 0 auto">
         <iframe width="100%" height="1500px"
             src="https://datastudio.google.com/embed/reporting/74f7c337-5a75-4ac3-8b93-e873e79fa0b4/page/fzFnB"
             frameborder="0" style="border:0" allowfullscreen>
-        </iframe>
-    </div>
+        </iframe>    
+    </div>    
 </div>
 
 @endsection
