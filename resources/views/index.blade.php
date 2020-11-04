4bd7b8e75120c87b0c3004b34182635e4efffc3c
satiya3577@gmail.com <satiya3577@gmail.com>
2020-11-04 16:46:33 +0700

    Test Dev Commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
 <div class="col-lg-12">
     <h2>ระบบรายงานข้อมูล</h2>
     <div class="col-lg-12" style="text-align: center;margin: 0 auto">
-        <iframe width="100%" height="800px"
-            src="https://datastudio.google.com/embed/reporting/74f7c337-5a75-4ac3-8b93-e873e79fa0b4/page/lppfB"
+        <iframe width="100%" height="1500px"
+            src="https://datastudio.google.com/embed/reporting/74f7c337-5a75-4ac3-8b93-e873e79fa0b4/page/fzFnB"
             frameborder="0" style="border:0" allowfullscreen>
         </iframe>
     </div>
@@ -14,10 +14,5 @@
 
 @endsection
 @section('script')
-<script>
-    $(document).ready(function () {
-        $('#example').DataTable();
-    });
 
-</script>
 @endsection
