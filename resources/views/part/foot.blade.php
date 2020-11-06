6d32fe3014d49aa59478f085703971ab16347a36
Yui Nakorn <yuinakorn@hotmail.com>
2020-11-06 15:09:47 +0700

    แก้ไข css กับอักษร

--- resources/views/part/foot.blade.php
+++ resources/views/part/foot.blade.php
@@ -1,9 +1,10 @@
 <footer class="footer mt-auto py-3">
     <div class="container" style="padding-top: 1%;">
-        <p class="pull-left text-secondary">
-            สำนักงานสาธารณสุขจังหวัดเชียงใหม่
-            <br />
-            &copy; 2020  Chiang Mai Provincial Public Health Office.
+        <p class="pull-left text-secondary copyright">CM - DASHBOARD</p>
+        <hr>
+        <p class="pull-left text-secondary copyright">
+            สำนักงานสาธารณสุขจังหวัดเชียงใหม่ &copy;2020 Chiangmai Public Health Office.
         </p>
     </div>
 </footer>
+
--- resources/views/part/nav.blade.php
+++ resources/views/part/nav.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="#">
             <img src="https://cmbis.chiangmaihealth.go.th/web/img/chiangmaihealth.ico" width="30" height="30"
                 class="d-inline-block align-top">
-            CMH : <small class="text-success"> Dashboard</small>
+            <span class="cm">CM-</span><span class="text-success">DASHBOARD</span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
